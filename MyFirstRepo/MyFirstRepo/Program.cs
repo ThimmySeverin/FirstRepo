9e3649c6c228a8f4e9201718a21c38ec2d5c74e5
Thimmy Severin <thimmy.severin@learnet.se>
2024-10-17 09:54:52 +0200

    ändrat

---
+++ MyFirstRepo/MyFirstRepo/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace MyFirstRepo
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, Anders");
+        }
+    }
+}
